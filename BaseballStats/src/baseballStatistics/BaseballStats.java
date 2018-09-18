956381ad3879356d737cb9c89dccedb18331236e
emilyevans <emilyevans@wifi-roaming-128-4-105-72.host.udel.edu>
2018-09-18 15:18:11 -0400

    Final push

--- BaseballStats/src/baseballStatistics/BaseballStats.java
+++ BaseballStats/src/baseballStatistics/BaseballStats.java
@@ -6,67 +6,96 @@ import java.util.Scanner;
 public class BaseballStats {
 	
 	//public constructor
-	public BaseballStats() {
-
+		public BaseballStats() {
 	}
-
+	
+	//Creates scanner to obtain input
+	Scanner scan = new Scanner (System.in);
+		
 	//will prompt the person for name and save to variable Name
 	public void getName(String name){
 		System.out.println("Enter your name: ");
-		Scanner scan = new Scanner(System.in);
-		String Name = scan.nextLine();
-		System.out.println("Your name is: "+ Name);
-		scan.close();
+		name = scan.nextLine();
+		System.out.println("Your name is: "+ name);
 	}
 	
-	public void getAtBat (int bats) {
-		
-	}
-	public void getHits (int hits) {
-		
+	// Gets how many At Bats
+	public void AtBat (int atbats) {
+		System.out.println("Enter in the number of At Bat: ");
+		int AtBat = scan.nextInt();
+		System.out.println("Your amount of at bats is: " + AtBat);
 	}
 	
-	public void getDoubles (int doubles) {
+	//Will get how many hits and print out amount
+	public void Hits (int hits) {
+		System.out.println("Enter in the number of hits: ");
+		int Hits = scan.nextInt();
+		System.out.println("Your amount of hits is: " + Hits);
+	}
+	
+	public void Doubles (int doubles) {
 		System.out.println("Enter in the amount of doubles: ");
-		Scanner scan = new Scanner(System.in);
 		int Doubles = scan.nextInt();
 		System.out.println("Your amount of doubles is: " + Doubles);
-		scan.close();
 	}
-	
-	public void getTriples (int triples) {
+
+	public void Triples (int triples) {
 		System.out.println("Enter in the amount of triples: ");
-		Scanner scan = new Scanner (System.in);
 		int Triples = scan.nextInt();
 		System.out.println("Your number of trips are: " + Triples);
-		scan.close();
-	}
-	
-	public void getHomeRuns (int homeRun) {
-		System.out.println("Enter in the amount of homeruns: ");
-		Scanner scan = new Scanner (System.in);
-		int homeruns = scan.nextInt();
-		System.out.println ("Your number of homeruns is: " + homeruns);
-		scan.close();
 	}
 
-	public void getRuns (int runs) {
+	public void HomeRuns (int homeRuns) {
+		System.out.println("Enter in the amount of homeruns: ");
+		int homeRuns1 = scan.nextInt();
+		System.out.println ("Your number of homeruns is: " + homeRuns1);
+	}
+
+	public void Runs (int runs) {
 		System.out.println("Enter in the amount of runs: ");
-		Scanner scan = new Scanner(System.in);
 		int Runs = scan.nextInt();
 		System.out.println("Your amount of runs: "+ Runs);
-		scan.close();
 	}
 
-	public void getWalks (int walks) {
+	public void Walks (int walks) {
 		System.out.println("Enter in the amount of walks: ");
+		int Walks = scan.nextInt();
+		System.out.println("Your amount of walks is: " + Walks);
 	}
 
-	public int battingAvg (int Hits, int AtBat) {
+	public void HitByPitch (int hitsbpitch) {
+		System.out.println("Enter in the amount of hits by pitch: ");
+		int HitsByPitch = scan.nextInt();
+		System.out.println("Your amount of hits by pitch are: " + HitsByPitch);
+	}
+	
+	// will prompt user for sacrifice flies
+	public void SacrificeFlies (int sacrificeFlies) {
+		System.out.println("Enter in the amount of sacrifice flies: ");
+		int SacrificeFlies = scan.nextInt();
+		System.out.println("Your amount of sacrifice flies are: " + SacrificeFlies);
+	}
+	
+	public double battingAvg (int Hits, int AtBat) {
 		int BattingAverage= (Hits/AtBat);
 		return BattingAverage;
 	}
+
+	public double onBasePercent (int Hits, int Walks, int HitByPitch, int AtBat,
+			int walks, int hitByPitch, int SacrificeFlies) {
+		double BasePercent = (Hits + Walks + HitByPitch)/(AtBat + Walks + HitByPitch +
+			SacrificeFlies);
+		return BasePercent;
+	}
 	
-	
-	
+	public double sluggingPercent (int OneB, int TwoB, int ThreeB, int HomeRuns, int AtBat) {
+		int sluggingPercent = ((OneB + (2*TwoB)+ (3*ThreeB) + (4*HomeRuns))/AtBat);
+		return sluggingPercent;
+	}
+
+	public double OnBaseSlugPercent (double onBasePercent, double sluggingPercent) {
+		double onBaseSlugPercent = (onBasePercent + sluggingPercent);
+		return onBaseSlugPercent;
+	}
+
 }
